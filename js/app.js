6665e29430733b75b139a926c511d9ea9cc1151f
Claudio Tocco <tocco.a.claudio@gmail.com>
2018-12-06 09:31:09 +0100

    changes to ui input

--- js/app.js
+++ js/app.js
@@ -1,35 +1,4 @@
-// input listeners for name
-document.getElementById("form-1").addEventListener("submit", addPlayer);
 
-function addPlayer(evt) {
-  evt.preventDefault();
-  let playerName = document.getElementById("inputPlayer1");
-  let playerNameValue = playerName.value;
-  console.log(playerNameValue);
-  document.getElementById(
-    "playerOneHeader"
-  ).textContent = `Player 1: ${playerNameValue}`;
-}
-
-document.getElementById("form-2").addEventListener("submit", addPlayer2);
-
-function addPlayer2(evt) {
-  evt.preventDefault();
-  let playerName = document.getElementById("inputPlayer2");
-  let playerNameValue = playerName.value;
-  console.log(playerNameValue);
-  document.getElementById(
-    "playerTwoHeader"
-  ).textContent = `Player 2: ${playerNameValue}`;
-}
-
-document.getElementById("reset").addEventListener("click", reset);
-
-function reset(evt) {
-  evt.preventDefault();
-  console.log("hello");
-  window.location.reload();
-}
 
 // Deck setup and methods
 class Deck {
